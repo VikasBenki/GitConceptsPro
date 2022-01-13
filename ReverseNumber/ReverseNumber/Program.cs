2cecb228d650f06edaf77984d584bfd3d3a46310
Vikasbenki <vikasmsbenki@gmail.com>
2022-01-13 19:40:50 +0530

    [VikasBenki] adding

---
+++ ReverseNumber/ReverseNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a Num");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Reversenumber.GetReverse(num);
+        }
+    }
+}
